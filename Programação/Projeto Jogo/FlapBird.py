5b53aa45a2094dfee369fbfb9e594aac1737f00a
castrox <marcelo.dengo@gmail.com>
2025-08-27 11:02:31 -0300

    Update FlapBird.py

--- Programação/Projeto Jogo/FlapBird.py
+++ Programação/Projeto Jogo/FlapBird.py
@@ -101,9 +101,19 @@ class Cano:
         self.definir_altura()
     def definir_altura(self):
         self.altura = random.randrange(50,450)
-        self.pos_base = self.altura-self.CANO_TOPO
+        self.pos_base = self.altura-self.CANO_TOPO.get_height()
         self.pos_topo = self.altura+self.DISTANCIA
-
+    def mover(self):
+        self.x = self.VELOCIDADE
+    def desenhar(self,tela):
+        tela.blit(self.CANO_TOPO, (self.x, self.pos_topo))
+        tela.blit(self.CANO_BASE, (self.x, self.pos_base))
+    def colidir(self, passaro):
+        passaro_mask = passaro.get_mask()
+        topo_mask = pygame.mask.from_surface(self.CANO_TOPO)
+        base_mask = pygame.mask.from_surface(self.CANO_BASE)
+        
+        
 class Chao:
     pass
 
